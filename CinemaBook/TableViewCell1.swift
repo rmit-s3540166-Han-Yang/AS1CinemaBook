d6aa08180047ea87c4f75d688e3ce20e32bf4e92
Han <han363@Hans-iMac.local>
2017-08-27 16:36:41 +1000

    v1.11

---
+++ CinemaBook/TableViewCell1.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell1.swift
+//  CinemaBook
+//
+//  Created by Han on 27/8/17.
+//  Copyright © 2017 Han. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieText: UITextView!
+}
