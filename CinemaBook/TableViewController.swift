57af38c06d5d8d7d04a482620f663aed03cf043e
Han <han363@Hans-iMac.local>
2017-08-27 17:31:30 +1000

    v1.12

--- CinemaBook/TableViewController.swift
+++ CinemaBook/TableViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
+struct cellData{
+    let cell : Int!
+    let text : String!
+    let image : UIImage!
+    
+}
+
 class TableViewController: UITableViewController {
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt IndexPath) -> UITableViewCell{
+        
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
 }
