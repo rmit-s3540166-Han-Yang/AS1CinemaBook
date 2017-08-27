aae2697a9832b9a6d33259e19df03c1b0decb37a
Han <han363@Hans-iMac.local>
2017-08-27 18:43:28 +1000

    images

--- CinemaBook/TableViewController.swift
+++ CinemaBook/TableViewController.swift
@@ -16,12 +16,26 @@ struct cellData{
 }
 
 class TableViewController: UITableViewController {
+    var arrayOfCellData = [cellData]()
+    
+    override func viewDidLoad() {
+        arrayOfCellData = [cellData(cell : 1, text : "", image : #imageLiteral(resourceName: "Capture2 copy")),
+        cellData(cell : 1, text : "", image : #imageLiteral(resourceName: "Capture2 copy")),
+        cellData(cell : 1, text : "", image : #imageLiteral(resourceName: "Capture2 copy"))]
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return arrayOfCellData.count
+        
     }
     override func tableView(_ tableView: UITableView, cellForRowAt IndexPath) -> UITableViewCell{
-        
+        if arrayOfCellData[IndexPath.row].cell == 1{
+            let cell = Bundle.main.loadNibNamed("TableViewCell1", owner: self, options: nil)?.first as! TableViewCell1
+            
+            cell.movieImage.image = arrayOfCellData[]
+        }else{
+            
+        }
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         <#code#>
